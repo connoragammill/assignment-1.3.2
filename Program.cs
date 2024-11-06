0f8e7505e7a4fbed4474132af78af53d75fe0c7d
conno <conno@CONSTRAPCUM>
2024-11-06 14:37:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace sort_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array Length");
+            string[] food = { "pizza", "burger", "icecream", "hotdog" };
+            Console.WriteLine(food.Length);
+            Console.WriteLine();
+            Array.Sort(food);
+            foreach (string i in food)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Array Length");
+            
+
+            int[] TopFavorites = {4,2,1,3};
+            Console.WriteLine(TopFavorites.Length);
+            Console.WriteLine();
+            Array.Sort(TopFavorites);
+            foreach (int i in TopFavorites)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+            
+            
+            
+
+            
+        }
+    }
+}
